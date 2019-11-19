9a283e7a006457ac4f3a22b4d9bd1fba2c8636b7
kramit <mike@michaelwhitehouse.com>
2019-11-19 21:50:24 +0000

    updating customscript

--- templates/vmcustomscript/customscript.ps1
+++ templates/vmcustomscript/customscript.ps1
@@ -1,1 +1,25 @@
-New-Item -ItemType Directory -Path c:\ -Name "test"
+
+# Set file and folder path for SSMS installer .exe
+$folderpath="$env:TEMP"
+$filepath="$folderpath\SSMS-Setup-ENU.exe"
+ 
+#If SSMS not present, download
+if (!(Test-Path $filepath)){
+write-host "Downloading SQL Server 2016 SSMS..."
+$URL = "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
+$clnt = New-Object System.Net.WebClient
+$clnt.DownloadFile($url,$filepath)
+Write-Host "SSMS installer download complete" -ForegroundColor Green
+ 
+}
+else {
+ 
+write-host "Located the SQL SSMS Installer binaries, moving on to install..."
+}
+ 
+# start the SSMS installer
+write-host "Beginning SSMS 2016 install..." -nonewline
+$Parms = " /Install /Quiet /Norestart /Logs log.txt"
+$Prms = $Parms.Split(" ")
+& "$filepath" $Prms | Out-Null
+Write-Host "SSMS installation complete" -ForegroundColor Green
