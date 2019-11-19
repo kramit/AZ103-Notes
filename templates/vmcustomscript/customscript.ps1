d5a2a234d1f4112078761a6ea80df3bb1c7cc29d
kramit <mike@michaelwhitehouse.com>
2019-11-19 23:00:49 +0000

    changing download to latest version

--- templates/vmcustomscript/customscript.ps1
+++ templates/vmcustomscript/customscript.ps1
@@ -6,7 +6,7 @@ $filepath="$folderpath\SSMS-Setup-ENU.exe"
 #If SSMS not present, download
 if (!(Test-Path $filepath)){
 write-host "Downloading SQL Server 2016 SSMS..."
-$URL = "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
+$URL = "https://download.microsoft.com/download/1/9/4/1949aa9c-6536-48f2-81fa-e7bb07410b36/SSMS-Setup-ENU.exe"
 $clnt = New-Object System.Net.WebClient
 $clnt.DownloadFile($url,$filepath)
 Write-Host "SSMS installer download complete" -ForegroundColor Green
@@ -18,7 +18,7 @@ write-host "Located the SQL SSMS Installer binaries, moving on to install..."
 }
  
 # start the SSMS installer
-write-host "Beginning SSMS 2016 install..." -nonewline
+write-host "Beginning SSMS install..." -nonewline
 $Parms = " /Install /Quiet /Norestart /Logs log.txt"
 $Prms = $Parms.Split(" ")
 & "$filepath" $Prms | Out-Null
