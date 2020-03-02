392dcbf0a53dab5ef72afaea5671aebe028ee082
Michael Whitehouse <mike@michaelwhitehouse.com>
2020-03-02 22:50:06 +0000

    fix comma

--- templates/mod6test/deploy.ps1
+++ templates/mod6test/deploy.ps1
@@ -4,11 +4,11 @@ Param(
     $location = 'eastus'
 )
 
-$rgNames = 'az104-06-rg01','az104-06-rg02''az104-06-rg03'
+$rgNames = 'az104-06-rg01','az104-06-rg02','az104-06-rg03'
 
 foreach ($rgname in $rgNames) {
 
-    Write-Host "Creating Resource Group $rgname in $location as a backround job"
+    Write-Host "Creating Resource Group $rgname in $location as a background job"
 New-AzResourceGroup -Name $rgName -Location $location
 
 New-AzResourceGroupDeployment `
