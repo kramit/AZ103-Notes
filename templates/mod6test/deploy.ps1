57f80b0860e5865cda8d930563372dd3f33ec04b
Michael Whitehouse <mike@michaelwhitehouse.com>
2020-03-02 23:12:40 +0000

    adding number suffix

--- templates/mod6test/deploy.ps1
+++ templates/mod6test/deploy.ps1
@@ -5,17 +5,22 @@ Param(
 )
 
 $rgNames = 'az104-06-rg01','az104-06-rg02','az104-06-rg03'
-
+$namesuffixcount = 0
 foreach ($rgname in $rgNames) {
 
-    Write-Host "Creating Resource Group $rgname in $location as a background job"
+    $namesuffixcount = $namesuffixcount++
+    Write-Host "Creating Resource Group $rgname in $location as a background job" -ForegroundColor Yellow
 New-AzResourceGroup -Name $rgName -Location $location
 
 New-AzResourceGroupDeployment `
    -ResourceGroupName $rgName `
    -TemplateFile "$pwd/az104-06-vms-template.json" `
    -TemplateParameterFile "$pwd/az104-06-vm-parameters.json" `
+   -nameSuffix $namesuffixcount
    -AsJob
-
-
 }
+
+Write-Host "Showing active powershell background jobs (note, this is a static output)"
+
+Get-Job
+
